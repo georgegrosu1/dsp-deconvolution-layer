459cdc5434b76e8ec77f0b51f0a4ef9a27a87990
georgegrosu1 <george.grosu.florin@gmail.com>
2022-06-08 17:41:48 +0200

    Typos, bugs and modifications on Deconvolution layer

--- tf_layers_extended/deconv.py
+++ tf_layers_extended/deconv.py
@@ -7,24 +7,22 @@ from nn_ops_extent.ops_extent import deconv1d
 
 
 class Deconvolution(Layer):
-
-    def __int__(self,
-                filters: int = None,
-                kernel_size=None,
-                padding=None,
-                activation=None,
-                use_bias=True,
-                kernel_initializer='glorot_uniform',
-                lambd_initializer='random_normal',
-                bias_initializer='zeros',
-                kernel_regularizer=None,
-                lambd_regularizer=None,
-                bias_regularizer=None,
-                activity_regularizer=None,
-                trainable=True,
-                name=None,
-                conv_op=None,
-                **kwargs):
+    def __init__(self,
+                 filters,
+                 kernel_size,
+                 padding=None,
+                 activation=None,
+                 use_bias=True,
+                 kernel_initializer='glorot_uniform',
+                 lambd_initializer='random_normal',
+                 bias_initializer='zeros',
+                 kernel_regularizer=None,
+                 lambd_regularizer=None,
+                 bias_regularizer=None,
+                 activity_regularizer=None,
+                 trainable=True,
+                 name=None,
+                 **kwargs):
         super(Deconvolution, self).__init__(
             trainable=trainable,
             name=name,
@@ -40,33 +38,11 @@ class Deconvolution(Layer):
         self.kernel_regularizer = regularizers.get(kernel_regularizer)
         self.lambd_regularizer = regularizers.get(lambd_regularizer)
         self.bias_regularizer = regularizers.get(bias_regularizer)
-        self.activity_regularizer = activity_regularizer
         self.use_bias = use_bias
         self.w_real = None
         self.w_imag = None
         self.s = None
         self.b = None
-
-    def _pad_input(self, input_tensor):
-        assert isinstance(self.padding, tuple) and len(self.padding) <= 2, 'Padding is specified by tuple with pad ' \
-                                                                           'length and mode (optionally)'
-        if tf.rank(input_tensor) == 1:
-            if len(self.padding) == 2:
-                return tf.concat(input_tensor, tf.zeros(shape=(self.padding[0], )), mode=self.padding[-1], axis=0)
-            return tf.concat(input_tensor, tf.zeros(shape=(self.padding[0], )), axis=0)
-        else:
-            if len(self.padding) == 2:
-                return tf.pad(input_tensor, ((0, 0), (0, self.padding[0])), mode=self.padding[-1])
-            return tf.pad(input_tensor, ((0, 0), (0, self.padding[0])))
-
-    def _match_filters_to_input_padding(self, input_shape):
-        if self.padding is not None:
-            len_pad = input_shape[-1] + self.padding[0] - self.w.shape[-1]
-        else:
-            len_pad = input_shape[-1] - self.w.shape[-1]
-        if tf.rank(self.w) == 1:
-            return tf.concat(self.w, tf.zeros(shape=(self.padding[0], ), dtype='float32'), axis=0)
-        return tf.pad(self.w, ((0, 0), (0, len_pad)), 'constant')
 
     def build(self, input_shape):
         self.w_real = self.add_weight(shape=(self.filters, self.kernel_size[0]),
@@ -93,7 +69,7 @@ class Deconvolution(Layer):
                                      trainable=True,
                                      dtype='float32')
 
-    def call(self, inputs, training=True):
+    def call(self, inputs, *args, **kwargs):
         x = tf.constant(inputs)
         # Apply padding to input if specified
         if self.padding is not None:
@@ -103,8 +79,29 @@ class Deconvolution(Layer):
         # Apply bias accordingly
         if self.use_bias:
             x = x + self.b
-        x = self.activation(x)
+        if self.activation is not None:
+            x = self.activation(x)
         return x
+
+    def _pad_input(self, input_tensor):
+        assert isinstance(self.padding, tuple) and len(self.padding) <= 2, 'Padding is specified by tuple with pad ' \
+                                                                           'length and mode (optionally)'
+        if tf.rank(input_tensor) == 1:
+            if len(self.padding) == 2:
+                return tf.concat(input_tensor, tf.zeros(shape=(self.padding[0], )), mode=self.padding[-1], axis=0)
+            return tf.concat(input_tensor, tf.zeros(shape=(self.padding[0], )), axis=0)
+        if len(self.padding) == 2:
+            return tf.pad(input_tensor, ((0, 0), (0, self.padding[0])), mode=self.padding[-1])
+        return tf.pad(input_tensor, ((0, 0), (0, self.padding[0])))
+
+    def _match_filters_to_input_padding(self, input_shape):
+        if self.padding is not None:
+            len_pad = input_shape[-1] + self.padding[0] - self.w.shape[-1]
+        else:
+            len_pad = input_shape[-1] - self.w.shape[-1]
+        if tf.rank(self.w) == 1:
+            return tf.concat(self.w, tf.zeros(shape=(self.padding[0], ), dtype='float32'), axis=0)
+        return tf.pad(self.w, ((0, 0), (0, len_pad)), 'constant')
 
     def _serialize_to_tensors(self):
         pass

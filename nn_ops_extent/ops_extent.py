c6bc176bbf3bbb2a60f3177e1101fa59f4afee68
georgegrosu1 <george.grosu.florin@gmail.com>
2022-06-08 16:34:38 +0200

    Bugs solved for deconv1d function

--- nn_ops_extent/ops_extent.py
+++ nn_ops_extent/ops_extent.py
@@ -6,22 +6,21 @@ def deconv1d(input_vect, filters, lambds):
     We take the Fourier transform of the input vector and the filter's transfer function, multiply them together, divide by
     the sum of the squared filter's transfer function and the regularization parameter, and then take the inverse Fourier
     transform of the result
-    
     :param input_vect: The input vector to be deconvolved
     :param filters: the filter's transfer function
     :param lambds: SNR values
     :return: The deconvolved signal.
     """
     # Complete possibly real input vector with zeros for imaginary parts
-    if all(tf.math.imag(input_vect)):
-        input_vect = tf.complex(input_vect, tf.zeros_like(input_vect, dtype='float32'))
+    if input_vect.dtype != tf.complex64:
+        input_vect = tf.cast(input_vect, tf.complex64)
+    if lambds.dtype != tf.complex64:
+        lambds = tf.cast(lambds, tf.complex64)
     # Generate the Fourier transforms of the input vectors and filter's transfer function
-    fft_input = tf.constant(tf.signal.fft(input_vect))
-    fft_filters = tf.constant(tf.signal.fft(tf.complex(filters[0], filters[-1])))
+    fft_input = tf.signal.fft(input_vect)
+    fft_filters = tf.signal.fft(tf.complex(filters[0], filters[-1]))
     # Compute simple Wiener deconvolution
-    deconvolved = tf.constant(tf.math.real(tf.signal.ifft(tf.divide(tf.multiply(fft_input, tf.math.conj(fft_filters)),
-                                                                    (tf.add(tf.multiply(fft_filters,
-                                                                                        tf.math.conj(fft_filters)),
-                                                                            tf.pow(lambds, 2)))))))
+    deconvolved = tf.math.real(tf.signal.ifft(fft_input * tf.math.conj(fft_filters) /
+                                              (fft_filters * tf.math.conj(fft_filters) + lambds**2)))
 
     return deconvolved

0d22be3ab521faaa70d11c84fc90b58011ed011f
georgegrosu1 <george.grosu.florin@gmail.com>
2022-06-17 14:32:03 +0200

    Added different bias value for each deconvoluted resulted feature

--- tf_layers_extended/deconv.py
+++ tf_layers_extended/deconv.py
@@ -67,6 +67,7 @@ class Deconvolution(Layer):
                                  trainable=True,
                                  dtype='float32')
         if self.use_bias:
+            self.b = self.add_weight(shape=(1, self.filters * input_shape[-1]),
             self.b = self.add_weight(shape=(1, ),
                                      initializer=self.bias_initializer,
                                      regularizer=self.bias_regularizer,

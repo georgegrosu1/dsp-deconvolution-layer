f0ab4e290da4f625e510273531edbc437431ac78
georgegrosu1 <george.grosu.florin@gmail.com>
2022-07-24 12:52:53 +0300

    Updated __init__ files

--- nn_ops_extent/__init__.py
+++ nn_ops_extent/__init__.py
@@ -1,1 +1,1 @@
-from nn_ops_extent.ops_extent import deconv1d, expand_tensor_dims_recursive, outer_elementwise
+from nn_ops_extent.ops_extent import deconv1d, deconv2d, expand_tensor_dims_recursive, outer_elementwise
